ab08377f7932af20338842c98722d76a6f5ad4e0
shawn gilroy <shawnpgilroy@gmail.com>
2024-06-07 09:33:40 -0400

    test final/

--- runner.R
+++ runner.R
@@ -1,12 +1,16 @@
 # Working to update?
-
 #install.packages("usethis")
 
+# Older OG stuff
 # usethis::use_github_action(
 #   url = "https://raw.githubusercontent.com/miyamot0/actions/v1/examples/release-file-system-image.yml"
 # )
 
+# usethis::use_github_action(
+#   url = "https://raw.githubusercontent.com/miyamot0/actions/main/examples/deploy-cran-repo.yml"
+# )
+
 usethis::use_github_action(
-  url = "https://raw.githubusercontent.com/miyamot0/actions/v1/examples/release-file-system-image.yml"
+  url = "https://raw.githubusercontent.com/miyamot0/actions/main/examples/release-file-system-image.yml"
 )
 
