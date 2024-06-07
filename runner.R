dee336939f72a08c6f87ba857c3efe15184e9c1d
shawn gilroy <shawnpgilroy@gmail.com>
2024-06-06 21:56:44 -0400

    Add

--- runner.R
+++ runner.R
@@ -1,5 +1,10 @@
 #install.packages("usethis")
 
+# usethis::use_github_action(
+#   url = "https://raw.githubusercontent.com/miyamot0/actions/v1/examples/release-file-system-image.yml"
+# )
+
 usethis::use_github_action(
   url = "https://raw.githubusercontent.com/miyamot0/actions/v1/examples/release-file-system-image.yml"
 )
+
